09acc25ff34c496d98b779c034baf77d46ff4ef6
Renan Braga <psico.bragarenan@gmail.com>
2025-04-11 08:31:08 -0300

    feat:add counter actor

---
+++ src/main/scala/CounterApp.scala
@@ -0,0 +1,59 @@
+import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+
+
+object CounterApp {
+
+  // Messages the actor can receive
+  sealed trait CounterCommand
+  case object Increment extends CounterCommand
+  case object GetCount extends CounterCommand
+  case class ShowCount(replyTo: akka.actor.typed.ActorRef[Int]) extends CounterCommand
+
+  object Counter {
+    def apply(): Behavior[CounterCommand] = counterBehavior(0)
+
+    def counterBehavior(count: Int): Behavior[CounterCommand] = Behaviors.receive { (context, message) =>
+      message match {
+        case Increment =>
+          context.log.info(s"Counter incremented by ${count + 1}")
+          counterBehavior(count + 1)
+        case ShowCount(replyTo) =>
+          replyTo ! count
+          Behaviors.same
+        case GetCount =>
+          context.log.info(s"O contador está em: $count")
+          Behaviors.same
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem(Counter(), "MessageCounterSystem")
+
+    system ! Increment
+    system ! Increment
+    system ! GetCount
+
+    import scala.concurrent.duration._
+    import system.executionContext
+
+    val printer = ActorSystem[Int](
+      Behaviors.receiveMessage[Int] { count =>
+        println(s"Contador atual: $count")
+        Behaviors.stopped
+      },
+      "Printer"
+    )
+
+    system ! ShowCount(printer)
+
+    system.scheduler.scheduleOnce(3.seconds, new Runnable {
+      override def run(): Unit = {
+        system.terminate()
+        printer.terminate()
+      }
+    })
+  }
+
+}
